d8f01f44880c2e4de3a828229fa267dc4d92eef6
laraameg <92958032+laraameg@users.noreply.github.com>
2023-05-01 15:08:07 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEto9iiwnbGYbv9urotQNYPTT1zvtgLhw",
+  authDomain: "historia-c36c7.firebaseapp.com",
+  databaseURL: "https://historia-c36c7-default-rtdb.firebaseio.com",
+  projectId: "historia-c36c7",
+  storageBucket: "historia-c36c7.appspot.com",
+  messagingSenderId: "910054691744",
+  appId: "1:910054691744:web:2d6facab865c7fba944ac5"
+};
